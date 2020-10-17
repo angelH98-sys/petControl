7bc21f0c3d4ddfb915acf47a6a68d7ad72fcabf9
Rolando988 <68891894+Rolando988@users.noreply.github.com>
2020-10-17 17:14:24 -0600

    petcontrol

---
+++ src/app/componentes/cliente/cliente.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertaService } from 'src/app/utilidades/alerta.service';
+import { DbService } from 'src/app/utilidades/db.service';
+import { ValidadorService } from 'src/app/utilidades/validador.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+  formcliente: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+    private validadorService: ValidadorService,
+    public alertaService: AlertaService,
+    private db: DbService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.formcliente = this.formBuilder.group({
+      id: undefined,
+      nombre: ['', Validators.required],
+      telefono: ['', Validators.required],
+      correo: ['', [
+        Validators.required,
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+      ]],
+      direccion: ['', Validators.required],
+      mascota: ['', Validators.required],  
+      dui: ['', Validators.required],  
+        
+    })
+  }
+  formGroupToUser(){
+    return {
+      nombre: this.formcliente.get('nombre').value,
+      telefono: this.formcliente.get('telefono').value,
+      correo: this.formcliente.get('correo').value,
+      direccion: (this.formcliente.get('direccion').value),
+      mascota: this.formcliente.get('mascota').value,
+      dui: this.formcliente.get('dui').value,
+     
+    }
+  }
+  async onSubmit(){
+    this.validadorService.verificarContrasenias(this.formcliente);
+    if(this.formcliente.invalid) return false;
+    try{
+      await this.db.Create(this.formGroupToUser(), 'cliente');
+      this.alertaService.openSuccessSnackBar('cliente registrado exitosamente');
+      this.router.navigate(['usuarios/tabla']);
+    }catch(rej){
+      this.alertaService
+        .openErrorSnackBar('Ups... algo salio mal al registrar el cliente.');
+    }
+  }
+
+}
