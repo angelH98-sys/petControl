cfd3f32c9bcbc895423788a7f220351943dfdc74
juan180401 <68620250+juan180401@users.noreply.github.com>
2020-10-18 20:53:22 -0600

    Formulario actualizado
    Se coloco de forma correcta el formulario en el menu de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormUsuarioComponent } from './componentes/usuario/form-usuario/form-usuario.component';
 import { TableUsuarioComponent } from './componentes/usuario/table-usuario/table-usuario.component';
+import{ ServiciosComponent} from './componentes/servicios/servicios.component';
 
 const routes: Routes = [
   { path: 'usuarios/registro', component: FormUsuarioComponent },
   { path: 'usuarios/tabla', component: TableUsuarioComponent },
+  { path: 'servicio/registro',component: ServiciosComponent}
 ];
 
 @NgModule({
