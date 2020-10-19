47a1c9a3808118d64ef407575fb7496072f45b8b
juan180401 <68620250+juan180401@users.noreply.github.com>
2020-10-18 23:39:01 -0600

    Ubicacion Formulario
    se arreglo la ubicacion del formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormUsuarioComponent } from './componentes/usuario/form-usuario/form-usuario.component';
 import { TableUsuarioComponent } from './componentes/usuario/table-usuario/table-usuario.component';
 import{ ServiciosComponent} from './componentes/servicios/servicios.component';
-
+//*import{ TableServicioComponent} from './componentes/servicios/table-servicio/table-servicio.component'
 const routes: Routes = [
   { path: 'usuarios/registro', component: FormUsuarioComponent },
   { path: 'usuarios/tabla', component: TableUsuarioComponent },
-  { path: 'servicio/registro',component: ServiciosComponent}
+  { path: 'servicio/registro',component: ServiciosComponent},
+ // { path: 'servicio/tabla', component: TableServicioComponent },
 ];
 
 @NgModule({
