cb4d1bb6112d03fd96faeb42e58436b1b01493a6
RicardoSolorzano0 <ricardosolorzano97@gmail.com>
2020-10-19 20:33:47 -0600

    tabla empleado
    Completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormEmpleadoComponent } from './componentes/empleado/form-empleado/form-empleado.component';
+import { TableEmpleadoComponent } from './componentes/empleado/table-empleado/table-empleado.component';
 import { FormUsuarioComponent } from './componentes/usuario/form-usuario/form-usuario.component';
 import { TableUsuarioComponent } from './componentes/usuario/table-usuario/table-usuario.component';
 
 const routes: Routes = [
   { path: 'usuarios/registro', component: FormUsuarioComponent },
   { path: 'usuarios/tabla', component: TableUsuarioComponent },
+  { path: 'empleados/tabla', component:TableEmpleadoComponent},
+  { path: 'empleados/registro', component:FormEmpleadoComponent},
 ];
 
 @NgModule({
